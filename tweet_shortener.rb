d3c1cd291841665c1440577557d6a721153a04e4
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-21 20:38:43 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,27 @@
 # Write your code here.
 
 def word_substituter(tweet)
-  arr = []
-  arr.push(tweet.split(" "))
-  puts arr
+  my_dict = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+  arr = tweet.split(" ")
+  keys_arr = my_dict.keys
+  new_arr = []
+  for i in 0...arr.length
+    if keys_arr.include? arr[i]
+      new_arr.push(my_dict["#{arr[i]}"])
+    else
+      new_arr.push(arr[i])
+    end
+  end
+  new_arr
 end
